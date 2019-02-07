075b0130b89a32edc1f218ff33c36fd81b864d98
晃弘 上山 <akihiro@hololab.co.jp>
2019-02-07 23:25:47 +0900

    first commit

---
+++ HoloLensLocalVoiceChat/Assets/LocalVoiceChat/Scripts/LocalVoiceChatManager.cs
@@ -0,0 +1,64 @@
+﻿using System;
+using System.Collections;
+using System.Collections.Generic;
+using HoloLensModule.Environment;
+using HoloLensModule.Network;
+using SaveWAVFile;
+using UnityEngine;
+
+namespace LocalVoiceChat
+{
+    /// <summary>
+    /// ローカルネットワーク内でUDPブロードキャストによるボイスチャットを行う
+    /// </summary>
+    public class LocalVoiceChatManager : MonoBehaviour
+    {
+        /// <summary>
+        /// 受信音声出力用
+        /// </summary>
+        [SerializeField] private AudioSource audioSource;
+
+        /// <summary>
+        /// Voice周波数
+        /// </summary>
+        [SerializeField] private int frequency = 16000;
+
+        /// <summary>
+        /// UDP通信用ポート
+        /// </summary>
+        [SerializeField] private int port = 8001;
+
+        /// <summary>
+        /// UDPネットワーク処理
+        /// </summary>
+        private UDPListenerManager listener;
+        private UDPSenderManager sender;
+
+        private WAVStreamSender voiceSender;
+
+        // Use this for initialization
+        void Start()
+        {
+            // Voice受信処理
+            var voiceListener = new WAVStreamListener(audioSource, frequency);
+            listener = new UDPListenerManager(port);
+            listener.ListenerByteEvent += (data, address) =>
+            {
+                if (SystemInfomation.IPAddress.Equals(address)) return;
+                voiceListener.SetDataList(data);
+            };
+           
+            // Voice送信処理
+            sender = new UDPSenderManager(SystemInfomation.DirectedBroadcastAddress, port);
+            voiceSender = new WAVStreamSender();
+            StartCoroutine(voiceSender.StartStreamRecordData(data => sender.SendMessage(data), frequency));
+        }
+
+        void OnDestroy()
+        {
+            voiceSender.StopStreamRecordData();
+            listener.DisConnectListener();
+            sender.DisConnectSender();
+        }
+    }
+}
--- README.md
+++ README.md
@@ -1,1 +1,23 @@
 # HoloLensLocalVoiceChat
+- Twitter : [@akihiro01051](https://twitter.com/akihiro01051)
+
+## 動作環境
+- Windows10 Creators Update
+- Unity 2017.4
+- Visual Studio 2017
+- HoloLens RS4
+- Windows MixedReality Device
+
+----------
+
+## 概要
+- ローカルネットワーク内でUDPを利用した簡易的なボイスチャットを行います．
+
+## 利用パッケージ
+- [HoloLensModule](https://github.com/akihiro0105/HoloLensModule)
+- [HoloLensSaveWAVFile](https://github.com/akihiro0105/HoloLensSaveWAVFile)
+
+## 内容
+- LocalVoiceChatSample
+    + パソコンとHoloLensでUDPブロードキャストによるボイスチャットサンプル
+    
